c6d77ab63d91104f32ab5e5ae2943f4d26ff875f
samypr100 <3933065+samypr100@users.noreply.github.com>
2025-10-24 21:52:54 -0400

    Third Revision

--- src/test_lfs_repo/__init__.py
+++ src/test_lfs_repo/__init__.py
@@ -1,2 +1,7 @@
 def main() -> None:
     print("Hello from test-lfs-repo!")
+
+def main_lfs() -> None:
+    from .lfs_module import LFS_TEST
+    from .another_lfs_module import ANOTHER_LFS_TEST
+    print(f"Hello from test-lfs-repo! {LFS_TEST=} {ANOTHER_LFS_TEST=}")
